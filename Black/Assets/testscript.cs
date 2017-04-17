52a1bb55e69fdd476c95cf613811b599d5e48a31
Grace Liu <msg.her@gmail.com>
2017-04-17 17:59:29 -0400

    Updating current scene Grace is working on - Gracemodifiedagain and added prefabs for some new objects

---
+++ Black/Assets/testscript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour {
+//	void OnControllerColliderHit(ControllerColliderHit hit){
+//			Debug.Log ("collison detected");
+//	}
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("collison detected");
+	}
+}
